d000823bcb51974a3c41570731c616107d1d3a3f
Igor <igor310597@gmail.com>
2016-04-02 20:48:12 -0300

    pip commit

---
+++ README.md
@@ -0,0 +1,9 @@
+#Machine Learning 
+
+##Link do pip
+
+https://pip.pypa.io/en/stable/installing
+
+##Comando para instalar o scikit
+
+sudo pip install scikit-learn
---
+++ classificacao.py
@@ -0,0 +1,40 @@
+#gordo perna curta  late
+
+porco1 = [1,1,0]
+porco2 = [1,1,0]
+porco3 = [1,1,0]
+
+cachorro1 = [1,1,1]
+cachorro2 = [0,1,1]
+cachorro3 = [0,1,1]
+
+dados = [porco1,porco2,porco3,cachorro1,cachorro2,cachorro3]
+
+marcacoes = [1,1,1,-1,-1,-1]
+
+
+
+from sklearn.naive_bayes import MultinomialNB
+
+modelo = MultinomialNB()
+
+modelo.fit(dados,marcacoes)
+misterioso1 = [1,1,0]
+misterioso2 = [1,1,1]
+misterioso3 = [0,0,1]
+
+marcacoes_teste = [1,-1,-1]
+teste = [misterioso1,misterioso2,misterioso3]
+
+resultado = modelo.predict(teste)
+diferenca = resultado - marcacoes_teste
+
+acertos = [d for d in diferenca if d==0]
+
+nacertos = len(acertos)
+nelementos = len(teste)
+
+
+
+
+
